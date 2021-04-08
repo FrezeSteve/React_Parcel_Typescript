326a29aad0a2c19ab9eba7c4cb4d0cb481c995b9
steve <stevefrisson@gmail.com>
2021-04-08 12:42:12 +0300

    starter template

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import "core-js/modules/es6.promise";
+import "core-js/modules/es6.array.iterator";
+import { App } from '~/ui/App';
+import { render } from 'react-dom';
+
+render(
+    <App/>,
+    document.getElementById('root')
+);
---
+++ src/ui/App.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+export class App extends Component {
+    public render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
